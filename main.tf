597403f7b8c780849c0c44a13b34a24ef98e5de9
vbronfman <vbronfman@users.noreply.github.com>
2023-12-08 00:06:12 +0200

    Update main.tf
    name of instance changed

--- main.tf
+++ main.tf
@@ -10,6 +10,6 @@ resource "aws_instance" "terraform-cloud-demo" {
   instance_type = var.instance_type
   subnet_id   = var.subnet_id
   tags = {
-   Name = "terraform-cloud-demo-server-${terraform.workspace}" 
+   Name = "terraform-cloud-demo-server-${terraform.workspace}-to-delete" 
   }
 }
